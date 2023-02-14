fedb89ca2fbcc6483f3fc4cc54097a30ffd4086f
unknown <you@example.com>
2023-02-14 10:38:36 -0300

    adição de textos e botões

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Adicionando um texto</Text>
+      <Text>Adicionando um cabeçalho</Text>
+      <Button title="enviar"> </Button>
+
       <StatusBar style="auto" />
     </View>
   );
